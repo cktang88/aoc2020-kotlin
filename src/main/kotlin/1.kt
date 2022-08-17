89c7f5bbf869fd6923057b3ab9638240c0a70a56
cktang88 <arctangent20@yahoo.com>
2022-08-17 14:28:28 -0500

    init

---
+++ src/main/kotlin/1.kt
@@ -0,0 +1,14 @@
+import java.io.File
+
+fun prob1() {
+    val fileName = "src/main/kotlin/1.txt"
+//    File(fileName).forEachLine { println(it) }
+    val nums = File(fileName).readLines().map(String::toInt)
+//    var mp = nums.map { it to 2020 - it }.toMap()
+    val mp = nums.associateBy { 2020 - it }
+
+    println(mp)
+//    println(mp)
+    val entry = mp.filter { (_,v) -> v in mp }.firstNotNullOf{it}
+    println(entry)
+}
